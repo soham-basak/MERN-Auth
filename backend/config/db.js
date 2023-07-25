b13d900681752b57ca3ab67e2a35e7cf9f7e7fc8
Dexter <sohambasak42@gmail.com>
2023-07-25 16:07:53 +0530

    mongodb config added

---
+++ backend/config/db.js
@@ -0,0 +1,13 @@
+import mongoose, { mongo } from "mongoose";
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGODB_URL);
+    console.log(`MongoDB connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(`MongoDB connection error: ${error.message}`);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
