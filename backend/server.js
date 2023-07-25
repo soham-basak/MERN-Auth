412e880903fab8be2314e787d1f508e5ddaa6feb
Dexter <sohambasak42@gmail.com>
2023-07-25 16:36:32 +0530

    user model & regiater, hashed functions added

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { urlencoded } from "express";
 import dotenv from "dotenv";
 import userRoutes from "./routes/userRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
@@ -9,6 +9,9 @@ connectDB();
 
 const port = process.env.PORT || 5000;
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/users", userRoutes);
 
