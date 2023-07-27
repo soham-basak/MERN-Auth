4dc6f16374fdb535c649b0c5278e5703752e2d8c
Dexter <sohambasak42@gmail.com>
2023-07-27 17:38:01 +0530

    logged out endpoint

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -58,7 +58,11 @@ const registerUser = asyncHandler(async (req, res) => {
 // route POST /api/users/logout
 // @access public
 const logoutUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Logout user" });
+  res.cookie("jwt", "", {
+    httpOnly: true,
+    expires: new Date(0),
+  });
+  res.status(200).json({ message: "User Logged Out" });
 });
 
 // @desc Get user progile
