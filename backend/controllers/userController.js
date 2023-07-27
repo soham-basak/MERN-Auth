25950a64f45c69d7a105a2f799322b2a4d4bb311
Dexter <sohambasak42@gmail.com>
2023-07-27 18:06:54 +0530

    Auth protect middleware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -69,7 +69,12 @@ const logoutUser = asyncHandler(async (req, res) => {
 // route GET /api/users/profile
 // @access private
 const getUserProfile = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "User profile" });
+  const user = {
+    _id: req.user._id,
+    name: req.user.name,
+    email: req.user.email,
+  };
+  res.status(200).json(user);
 });
 
 // @desc Update user progile
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,4 +1,6 @@
 import express from "express";
+const router = express.Router();
+import { protect } from "../middleware/authMiddleware.js";
 import {
   authUser,
   registerUser,
@@ -6,12 +8,11 @@ import {
   getUserProfile,
   updateUserProfile,
 } from "../controllers/userController.js";
-const router = express.Router();
 
 router.post("/", registerUser);
 router.post("/auth", authUser);
 router.post("/logout", logoutUser);
-router.get("/profile", getUserProfile);
-router.put("/profile", updateUserProfile);
+router.get("/profile", protect, getUserProfile);
+router.put("/profile", protect, updateUserProfile);
 
 export default router;
