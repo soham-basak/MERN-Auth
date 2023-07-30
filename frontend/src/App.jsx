23164c10f9590c53e85458e9ce4b071ceb4804b6
Dexter <sohambasak42@gmail.com>
2023-07-30 19:22:54 +0530

    Header & Home template

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import Header from "./components/Header";
+import { Outlet } from "react-router-dom";
+import { Container } from "react-bootstrap";
 
 const App = () => {
   return (
     <>
-      <h1>Hello</h1>
+      <Header />
+      <Container className="my-2">
+        <Outlet />
+      </Container>
     </>
   );
 };
