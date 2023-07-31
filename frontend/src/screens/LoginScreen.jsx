79edffef7bc27e23e45b83d83a9acc04fdcea6bc
Dexter <sohambasak42@gmail.com>
2023-07-31 12:31:21 +0530

    login & register screen added

---
+++ frontend/src/screens/LoginScreen.jsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { Form, Button, Row, Col } from "react-bootstrap";
+import FormContainer from "../components/FormContainer";
+
+const LoginScreen = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log("submit");
+  };
+
+  return (
+    <FormContainer>
+      <h1>Sign In</h1>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className="my-2" controlId="email">
+          <Form.Label>Email Address</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+        <Form.Group className="my-2" controlId="password">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Enter password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+
+        <Button variant="primary" type="submit">
+          Sign In
+        </Button>
+        <Row className="py-3">
+          <Col>
+            New User? <Link to="/register">Register</Link>
+          </Col>
+        </Row>
+      </Form>
+    </FormContainer>
+  );
+};
+
+export default LoginScreen;
---
+++ frontend/src/screens/RegisterScreen.jsx
@@ -0,0 +1,72 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { Form, Button, Row, Col } from "react-bootstrap";
+import FormContainer from "../components/FormContainer";
+
+const RegisterScreen = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log("submit");
+  };
+
+  return (
+    <FormContainer>
+      <h1>Sign Up</h1>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className="my-2" controlId="name">
+          <Form.Label>Name</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter your name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+
+        <Form.Group className="my-2" controlId="email">
+          <Form.Label>Email Address</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+
+        <Form.Group className="my-2" controlId="password">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+
+        <Form.Group className="my-2" controlId="password">
+          <Form.Label>Confirm Password</Form.Label>
+          <Form.Control
+            type="password"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+
+        <Button variant="primary" type="submit">
+          Sign In
+        </Button>
+        <Row className="py-3">
+          <Col>
+            Already have an account? <Link to="/login">Login</Link>
+          </Col>
+        </Row>
+      </Form>
+    </FormContainer>
+  );
+};
+
+export default RegisterScreen;
