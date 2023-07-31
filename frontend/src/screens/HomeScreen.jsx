6dde5e4d979855acf1ecba7cb9c34854125337ad
Dexter <sohambasak42@gmail.com>
2023-07-31 16:24:43 +0530

    feat(core): update user profile

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import Hero from "../components/Hero";
+import { useSelector } from "react-redux";
 
 const HomeScreen = () => {
-  return (
-    <>
-      <Hero />
-    </>
-  );
+  const { userInfo } = useSelector((state) => state.auth);
+  return <>{userInfo ? <h1>Hello {userInfo.name}</h1> : <Hero />}</>;
 };
 
 export default HomeScreen;
---
+++ frontend/src/screens/ProfileScreen.jsx
@@ -0,0 +1,102 @@
+import { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { Form, Button } from "react-bootstrap";
+import FormContainer from "../components/FormContainer";
+import { useDispatch, useSelector } from "react-redux";
+import { setCredentials } from "../slices/authSlice";
+import Loader from "../components/Loader";
+import { useUpdateUserMutation } from "../slices/usersApiSlice";
+import { toast } from "react-toastify";
+
+const ProfileScreen = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const { userInfo } = useSelector((state) => state.auth);
+
+  const [updateProfile, { isLoading }] = useUpdateUserMutation();
+
+  useEffect(() => {
+    setName(userInfo.name);
+    setEmail(userInfo.email);
+  }, [userInfo.name, userInfo.email]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (password !== confirmPassword) {
+      toast.error("Passwords is incorrect");
+    } else {
+      try {
+        const res = await updateProfile({
+          _id: userInfo._id,
+          name: name,
+          email: email,
+          password: password,
+        }).unwrap();
+        dispatch(
+          setCredentials({
+            ...res,
+          })
+        );
+        toast.success("Profile updated successfully");
+      } catch (err) {
+        toast.error(err?.data?.message || err.error);
+      }
+    }
+  };
+  return (
+    <FormContainer>
+      <h1>Update Profile</h1>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className="my-2" controlId="name">
+          <Form.Label>Name</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter your name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+
+        <Form.Group className="my-2" controlId="email">
+          <Form.Label>Email Address</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+
+        <Form.Group className="my-2" controlId="password">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+
+        <Form.Group className="my-2" controlId="password">
+          <Form.Label>Confirm Password</Form.Label>
+          <Form.Control
+            type="password"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+
+        {isLoading && <Loader />}
+        <Button variant="primary" type="submit">
+          Update
+        </Button>
+      </Form>
+    </FormContainer>
+  );
+};
+export default ProfileScreen;
